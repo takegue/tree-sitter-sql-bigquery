abe3b665dbe66434834bc8fb7cb7a072f1fc6444
TKNGUE <takegue@gmail.com>
2022-05-24 06:33:34 +0900

    fix reference

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -19,7 +19,7 @@ void Init(Local<Object> exports, Local<Object> module) {
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
   Nan::SetInternalFieldPointer(instance, 0, tree_sitter_sql_bigquery());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("sql").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("sql_bigquery").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
