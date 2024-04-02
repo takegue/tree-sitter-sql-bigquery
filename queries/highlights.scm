1c29e40fde4285bb485f651c3e8ad074a27c5ced
ilyakochik <ilya.kochik@me.com>
2024-04-02 09:03:57 +0100

    fix: change order so more specific patterns are captured

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,14 +1,24 @@
 ; constant
-
-[
-    "NULL"
-    "TRUE"
-    "FALSE"
-] @constant
+["NULL" "TRUE" "FALSE"] @constant
 (string) @string
 (number) @number
 (comment) @comment
 
+; types
+(struct) @type.builtin
+(array) @type.builtin
+(interval) @type.builtin
+(type_identifier) @type.builtin
+(system_variable) @variable.system
+
+(type) @type.builtin
+(column_type) @type.builtin
+(identifier) @variable
+
+(option_item key: (identifier) @variable.parameter)
+
+(as_alias alias_name: (identifier) @property)
+[";" "."] @punctuation.delimiter
 
 ; functions
 (function_call function: (identifier) @function)
@@ -16,32 +26,10 @@
 
 (call_statement routine_name: (identifier) @function)
 
-; types
-(struct) @type.builtin
-(array) @type.builtin
-(interval) @type.builtin
-(type_identifier) @type.builtin
-
-(option_item key: (identifier) @variable.parameter)
-(type) @type.builtin
-(column_type) @type.builtin
-(identifier) @variable
-
-(as_alias
-  alias_name: (identifier) @property)
-
 [
-  ";"
-  "."
-] @punctuation.delimiter
-
-
-(system_variable) @variable.system
-
-[
-    "_PARTITIONDATE"
-    "_PARTITIONTIME"
-    "_TABLE_SUFFIX"
+  "_PARTITIONDATE"
+  "_PARTITIONTIME"
+  "_TABLE_SUFFIX"
 ] @variable.builtin
 
 [
@@ -78,70 +66,70 @@
 
 ; keywords
 [
- "ALL"
- "AND"
- "AS"
- "ASC"
- "BETWEEN"
- "CASE"
- "CAST"
- "CREATE"
- "CREATE_SCHEMA"
- "DROP_SCHEMA"
- "ALTER_SCHEMA"
- "ALTER"
- "TABLE"
- "VIEW"
- "DESC"
- "DISTINCT"
- "ELSE"
- "END"
- "EXCEPT"
- "FALSE"
- "FOLLOWING"
- "FOR"
- "FROM"
- "FULL"
- "HAVING"
- "IF_EXISTS"
- "IF_NOT_EXISTS"
- "IN"
- "INNER"
- "INTERVAL"
- "INTO"
- "IS"
- "JOIN"
- "LEFT"
- "LIKE"
- "LIMIT"
- "MERGE"
- "NOT"
- "NULL"
- "ON"
- "OPTIONS"
- "OR"
- "OR_REPLACE"
- "OUTER"
- "OVER"
- "PARTITION_BY"
- "PRECEDING"
- "QUALIFY"
- "RANGE"
- "RIGHT"
- "ROLLUP"
- "ROWS"
- "SELECT"
- "SET"
- "TABLE"
- "THEN"
- "TO"
- "TRUE"
- "UNNEST"
- "USING"
- "WHEN"
- "WHERE"
- "GROUP_BY"
- "ORDER_BY"
- "WINDOW"
- "WITH"
+  "ALL"
+  "AND"
+  "AS"
+  "ASC"
+  "BETWEEN"
+  "CASE"
+  "CAST"
+  "CREATE"
+  "CREATE_SCHEMA"
+  "DROP_SCHEMA"
+  "ALTER_SCHEMA"
+  "ALTER"
+  "TABLE"
+  "VIEW"
+  "DESC"
+  "DISTINCT"
+  "ELSE"
+  "END"
+  "EXCEPT"
+  "FALSE"
+  "FOLLOWING"
+  "FOR"
+  "FROM"
+  "FULL"
+  "HAVING"
+  "IF_EXISTS"
+  "IF_NOT_EXISTS"
+  "IN"
+  "INNER"
+  "INTERVAL"
+  "INTO"
+  "IS"
+  "JOIN"
+  "LEFT"
+  "LIKE"
+  "LIMIT"
+  "MERGE"
+  "NOT"
+  "NULL"
+  "ON"
+  "OPTIONS"
+  "OR"
+  "OR_REPLACE"
+  "OUTER"
+  "OVER"
+  "PARTITION_BY"
+  "PRECEDING"
+  "QUALIFY"
+  "RANGE"
+  "RIGHT"
+  "ROLLUP"
+  "ROWS"
+  "SELECT"
+  "SET"
+  "TABLE"
+  "THEN"
+  "TO"
+  "TRUE"
+  "UNNEST"
+  "USING"
+  "WHEN"
+  "WHERE"
+  "GROUP_BY"
+  "ORDER_BY"
+  "WINDOW"
+  "WITH"
 ] @keyword
