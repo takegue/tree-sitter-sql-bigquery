ee2e17dd78458452808313ffd4e50bbfc04d700e
takegue <takegue@gmail.com>
2024-05-12 14:59:39 +0900

    fix: test format

--- bindings/python/tests/test_binding.py
+++ bindings/python/tests/test_binding.py
@@ -1,6 +1,9 @@
+import unittest
+
 from tree_sitter import Language, Parser
 import tree_sitter_sql_bigquery as tssql
 
-def test_binding():
-    SQL_LANGUAGE = Language(ts_sql_bigquery.language(), "sql")
-    assert language is not None
+class TestTS(unittest.TestCase):
+    def test_binding(self):
+        language = Language(tssql.language(), "sql")
+        assert language is not None
