f70d604404242d0e2084e603b54dd65a3740abaf
takegue <takegue@gmail.com>
2024-05-27 06:36:56 +0900

    fix: invalid test case

--- bindings/python/tests/test_binding.py
+++ bindings/python/tests/test_binding.py
@@ -5,5 +5,5 @@ import tree_sitter_sql_bigquery as tssql
 
 class TestTS(unittest.TestCase):
     def test_binding(self):
-        language = Language(tssql.language(), "sql")
+        language = Language(tssql.language())
         assert language is not None
